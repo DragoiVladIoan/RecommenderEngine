4119c6a40c68ae7919d0c05ff6b6ac5df6f28a8d
VladDragoi <vdrag01@safeway.com>
2020-05-31 22:32:48 +0300

    Remove unnecessary comments

--- RecommenderEngine/recommender_models/SparkCollaborativeFiltering.py
+++ RecommenderEngine/recommender_models/SparkCollaborativeFiltering.py
@@ -36,14 +36,9 @@ CustomerID - user id
 
 (training, test) = model_df.randomSplit([0.8, 0.2])
 
-#make stock code be a number
-#would like to concatenate fields which are the same
-#abs the quanity of -
 
 
 
-# Build the recommendation model using ALS on the training data
-# Note we set cold start strategy to 'drop' to ensure we don't get NaN evaluation metrics
 als = ALS(maxIter=10, regParam=0.01, implicitPrefs=True, userCol="CustomerID", itemCol="StockCode", ratingCol='Quantity_Scaled',
           coldStartStrategy="drop")
 
@@ -51,7 +46,6 @@ model = als.fit(training)
 model.save(model_path)
 
 
-# Evaluate the model by computing the RMSE on the test data
 predictions = model.transform(test)
 evaluator = RegressionEvaluator(metricName="rmse", labelCol="Quantity_Scaled",
                                 predictionCol="prediction")
